fe97b6c47d378c99a9da4b24eb79e9a1bd9f5333
shubhra tiwary <shubhratiwary5@gmail.com>
2025-07-16 03:20:19 +0530

    start with the minting logic

--- smart-contracts/contracts/ShivraiToken.sol
+++ smart-contracts/contracts/ShivraiToken.sol
@@ -8,7 +8,22 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // import "hardhat/console.sol";
 
 contract ShivraiToken is ERC20, Ownable {
-    constructor() ERC20("ShivraiToken", "Shivrai") Ownable(msg.sender) {
+    uint public maxAmount = 50000 * 10 ** 18;
+    uint public coolDown = 1 hours;
+    struct User {
+        uint amount;
+        uint lastMintTime;
+    }
+    mapping(address => User) public userMapping;
+    constructor() ERC20("ShivraiToken", "MRAJ") Ownable(msg.sender) {
         // no pre-supply minting
+    }
+    function faucetMint(address userAddress, uint amount) external {
+        User memory user = userMapping[userAddress];
+        require(
+            user.amount < maxAmount,
+            "user has exceeded the Limit for the maximum amount"
+        );
+        _mint(userAddress, amount * 10 ** decimals());
     }
 }
