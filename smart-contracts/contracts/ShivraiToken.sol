d46e21653fd8b6156382dc2b9e4d91740e10faec
shubhra tiwary <shubhratiwary5@gmail.com>
2025-07-17 05:30:16 +0530

    raceReward logic made

--- smart-contracts/contracts/ShivraiToken.sol
+++ smart-contracts/contracts/ShivraiToken.sol
@@ -7,25 +7,38 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 contract ShivraiToken is ERC20, Ownable {
-    uint public constant TOTAL_SUPPLY_CAP = 2_000_000 * 10 ** 18;
-    uint public COOLDOWN = 24 hours;
-    uint public MAX_AMOUNT_PER_USER = 20000 * 10 ** 18;
+    uint256 public immutable TOTAL_SUPPLY_CAP;
+    uint256 public COOLDOWN = 24 hours;
+    uint256 public RACE_COOLDOWN = 2 hours;
+    uint256 public MAX_AMOUNT_PER_USER = 20000 * 10 ** decimals();
     struct User {
         uint amount;
         uint lastMintTime;
+        RacePosition lastRoundPosition;
+        uint lastRaceTime;
+    }
+    enum RacePosition {
+        first,
+        second,
+        third
     }
     mapping(address => User) public userMapping;
     constructor() ERC20("ShivraiToken", "MRAJ") Ownable(msg.sender) {
         // no pre-supply minting
+        TOTAL_SUPPLY_CAP = 2_000_000 * 10 ** decimals();
     }
     function faucetMint(address userAddress) external {
         User storage user = userMapping[userAddress];
-        uint amount = MAX_AMOUNT_PER_USER - user.amount > 10
+        uint256 amount = MAX_AMOUNT_PER_USER - user.amount > 10
             ? 10
             : MAX_AMOUNT_PER_USER - user.amount;
-
+        uint256 tokenAmount = amount * 10 ** decimals();
         require(
-            totalSupply() + (amount * 10 ** decimals()) <= TOTAL_SUPPLY_CAP,
+            amount > 0,
+            "invalid condition can't mint 0 tokens, No tokens left"
+        );
+        require(
+            totalSupply() + tokenAmount <= TOTAL_SUPPLY_CAP,
             "Over the Limit of total supply cap"
         );
         require(
@@ -33,8 +46,37 @@ contract ShivraiToken is ERC20, Ownable {
             "You can't mine any tokens for now"
         );
 
-        _mint(userAddress, amount * 10 ** decimals());
-        user.amount += amount * 10 ** decimals();
+        _mint(userAddress, tokenAmount);
+        user.amount += tokenAmount;
         user.lastMintTime = block.timestamp;
+    }
+    function raceReward(address userAddress, RacePosition _position) external {
+        uint256 amount;
+        User storage user = userMapping[userAddress];
+        if (_position == RacePosition.first) {
+            amount = 20;
+        } else if (_position == RacePosition.second) {
+            amount = 10;
+        } else {
+            amount = 5;
+        }
+        uint256 reward = amount * 10 ** decimals();
+        require(
+            block.timestamp > user.lastRaceTime + RACE_COOLDOWN,
+            "You can't mine any tokens for now"
+        );
+        require(
+            user.amount + reward <= MAX_AMOUNT_PER_USER,
+            "Limit for the user max amount exceeded"
+        );
+        require(
+            totalSupply() + reward <= TOTAL_SUPPLY_CAP,
+            "Over the Limit of total supply cap"
+        );
+        _mint(userAddress, reward);
+        user.amount += reward;
+        user.lastMintTime = block.timestamp;
+        user.lastRaceTime = block.timestamp;
+        user.lastRoundPosition = _position;
     }
 }
