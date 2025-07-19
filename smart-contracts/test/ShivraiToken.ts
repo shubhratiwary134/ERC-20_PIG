cef5de311bac87e0ca3ac1717b8061545a0e7ef6
shubhra tiwary <shubhratiwary5@gmail.com>
2025-07-19 18:53:31 +0530

    beforeEach hook for the faucet introduced

--- smart-contracts/test/ShivraiToken.ts
+++ smart-contracts/test/ShivraiToken.ts
@@ -1,2 +1,11 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
+describe("ShivraiToken", () => {
+  let faucet, owner, user;
+  beforeEach(async function () {
+    [owner, user] = await ethers.getSigners();
+    const Faucet = await ethers.getContractFactory("ShivraiToken");
+    faucet = await Faucet.deploy();
+    await faucet.deployed();
+  });
+});
