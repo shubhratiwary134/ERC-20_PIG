2fdb50f6f864e97ec735a26019207b3816426e0c
shubhra tiwary <shubhratiwary5@gmail.com>
2025-07-25 04:05:38 +0530

    getContract util function logic complete . contract local deployment hardhat

--- frontend/src/utils/getContract.ts
+++ frontend/src/utils/getContract.ts
@@ -1,6 +1,13 @@
 import { ethers } from "ethers";
+import { FAUCET_CONTRACT_ADDRESS } from "../config/configContract";
+import FaucetJson from "../abi/Faucet.json";
 
 export function getContract(signerOrProvider: ethers.Signer | ethers.Provider) {
   // function to return the instance of the contract with signer or provider passed .
-  return signerOrProvider;
+  const contract = new ethers.Contract(
+    FAUCET_CONTRACT_ADDRESS,
+    FaucetJson.abi,
+    signerOrProvider
+  );
+  return contract;
 }
