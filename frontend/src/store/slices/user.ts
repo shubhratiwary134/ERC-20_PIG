160f65e93ae0f2c7c5f66c39f6925e0e41cca1d8
shubhra tiwary <shubhratiwary5@gmail.com>
2025-07-25 00:14:28 +0530

    created the userslice and used union types + const maps for the position enum .

---
+++ frontend/src/store/slices/user.ts
@@ -0,0 +1,33 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { RacePosition } from "../../types/types";
+
+interface userInterface {
+  amount: number;
+  lastMintTimeStamp: Date | null;
+  lastRacePosition: RacePosition;
+  lastRaceTimeStamp: Date | null;
+}
+const initialState: userInterface = {
+  amount: 0,
+  lastMintTimeStamp: null,
+  lastRacePosition: "none",
+  lastRaceTimeStamp: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<userInterface>) => {
+      const { amount, lastMintTimeStamp, lastRacePosition, lastRaceTimeStamp } =
+        action.payload;
+      state.amount = amount;
+      state.lastMintTimeStamp = lastMintTimeStamp;
+      state.lastRacePosition = lastRacePosition;
+      state.lastRaceTimeStamp = lastRaceTimeStamp;
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;
---
+++ frontend/src/types/types.ts
@@ -0,0 +1,8 @@
+// types.ts
+export type RacePosition = "first" | "second" | "third" | "none";
+
+export const enumMap: Record<number, RacePosition> = {
+  0: "first",
+  1: "second",
+  2: "third",
+};
