b68ea9e7d72238cd16cf26c97c78bdc351c971e0
shubhra tiwary <shubhratiwary5@gmail.com>
2025-08-11 22:45:01 +0530

    responsiveness

--- frontend/src/components/RaceCard.tsx
+++ frontend/src/components/RaceCard.tsx
@@ -15,32 +15,36 @@ const RaceCard = ({
   return (
     <div className="mt-10 flex flex-col shrink">
       <div className="font-oxanium">
-        <h1 className="text-xl lg:mt-5 lg:text-4xl lg:text-start text-center font-extrabold text-purple-400">
+        <h1 className="text-2xl sm:text-3xl lg:mt-5 lg:text-4xl lg:text-start text-center font-extrabold text-purple-400">
           SELECT YOUR CHAMPION
         </h1>
         <p className="text-sm lg:text-lg text-slate-400 mt-1 text-center lg:text-start">
           A legend will be crowned in the grand arena.
         </p>
       </div>
-      <div className="mt-10 grid grid-cols-2  lg:flex lg:gap-10 2xl:gap-20 gap-5 ">
+      <div
+        className="mt-10 grid grid-cols-[auto_auto] justify-between gap-y-5 
+             sm:grid-cols-[auto_auto_auto]
+             lg:flex md:grid-cols-[auto_auto_auto_auto]  lg:gap-10 2xl:gap-20"
+      >
         {Pigs.map((pig, index) => (
           <div
             key={index}
             onClick={() => handleClick(pig)}
             className={`flex ${
               selectedIndex == index ? "bg-white/20 scale-110" : "bg-white/10"
-            }  flex-col gap-5 items-center w-32 h-48 lg:w-64 lg:h-80
+            }  flex-col gap-3 lg:gap-5 items-center max-w-36 max-h-48 pb-5 lg:max-w-64 lg:max-h-80
             backdrop-blur-lg border border-white/20 rounded-2xl shadow-lg text-white transition-all
             duration-300 hover:cursor-pointer hover:scale-110 hover:bg-white/20 hover:border-white/40`}
           >
             <img
               src={pig.imageUrl}
-              className="rounded-2xl lg:rounded-none lg:h-1/2"
+              className="rounded-t-2xl lg:rounded-none lg:h-1/2"
             />
 
-            <p className="text-gray-200 text-sm lg:text-lg">{pig.name}</p>
+            <p className="text-gray-200 text-sm lg:text-lg ">{pig.name}</p>
             <div className="hidden lg:block h-1 bg-gray-400/50 w-full"></div>
-            <div className="hidden md:flex justify-between w-full px-5 text-gray-400 ">
+            <div className="hidden lg:flex justify-between w-full px-5 text-gray-400 ">
               <div className="flex flex-col gap-2 ">
                 <p className="text-sm ">Height: {pig.height}</p>
                 <p className="text-sm">Weight: {pig.weight}</p>
