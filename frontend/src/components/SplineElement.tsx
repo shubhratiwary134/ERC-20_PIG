bfa0a800d5257275cdd482e8d6df8781fc4193ed
shubhra tiwary <shubhratiwary5@gmail.com>
2025-09-07 16:45:32 +0530

    element changed

--- frontend/src/components/SplineElement.tsx
+++ frontend/src/components/SplineElement.tsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 const SplineElement = () => {
   return (
     <div style={{ width: "100%", height: "100%" }}>
-      <Spline scene="https://prod.spline.design/YzBW00DUVHIpn16B/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/oXwaFNjrzTNtp3NB/scene.splinecode" />
     </div>
   );
 };
