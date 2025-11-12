46e2b59258c32fc69ac74bcdf51ffeb2127d8fed
shubhra tiwary <shubhratiwary5@gmail.com>
2025-11-12 14:29:49 +0530

    updated the contract config and correctly imported the abi

--- frontend/src/utils/getContract.ts
+++ frontend/src/utils/getContract.ts
@@ -1,12 +1,12 @@
-import { ethers } from "ethers";
-import { FAUCET_CONTRACT_ADDRESS } from "../config/configContract";
-import FaucetJson from "../abi/Faucet.json";
+import { ethers } from 'ethers';
+import { FAUCET_CONTRACT_ADDRESS } from '../config/configContract';
+import abi from '../abi/Faucet.json';
 
 export function getContract(signerOrProvider: ethers.Signer | ethers.Provider) {
   // function to return the instance of the contract with signer or provider passed .
   const contract = new ethers.Contract(
     FAUCET_CONTRACT_ADDRESS,
-    FaucetJson.abi,
+    abi,
     signerOrProvider
   );
   return contract;
